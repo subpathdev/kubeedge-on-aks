4f0ca861dca46c04eb517e9d6ce1334fe55248a5
Armin Schlegel <armin.schlegel@gmx.de>
2021-07-30 13:54:21 +0200

    WIP

---
+++ edge/edgecore/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl get secrets casecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cadata' > config/ca/rootCa.crt
+kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcoredata' > config/certs/server.crt
+kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcorekeydata' > config/certs/server.key
+IP=`kubectl get service cloudcore -n kubeedge -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+echo CLOUDCORE_IP=${IP} > template/values.conf
+TOKEN=`kubectl get secrets tokensecret -n kubeedge -o jsonpath='{.data}' | jq -r '.tokendata' | base64 -d`
+echo TOKEN="\"${TOKEN}\"" >> template/values.conf
+
+docker build -t edge .
