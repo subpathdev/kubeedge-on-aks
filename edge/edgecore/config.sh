48d5ccf1ef35f28ceb455e1ef648038a855a49e4
Armin Schlegel <armin.schlegel@gmx.de>
2021-08-19 16:21:49 +0200

    replica 2

--- edge/edgecore/config.sh
+++ edge/edgecore/config.sh
@@ -3,7 +3,7 @@ kubectl get secrets casecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cadata'
 kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcoredata' > config/certs/server.crt
 kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcorekeydata' > config/certs/server.key
 #IP=`kubectl get service cloudcore -n kubeedge -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-IP=kubeedge.tenant1.edgefarm.dev
+IP=my-tenant.kubeedge.edgefarm.dev
 echo CLOUDCORE_IP=${IP} > template/values.conf
 TOKEN=`kubectl get secrets tokensecret -n kubeedge -o jsonpath='{.data}' | jq -r '.tokendata' | base64 -d`
 echo TOKEN="\"${TOKEN}\"" >> template/values.conf
