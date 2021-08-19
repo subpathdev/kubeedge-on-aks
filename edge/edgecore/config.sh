70271099745318c2414a23677bfaaa834a1fe3d7
Armin Schlegel <armin.schlegel@gmx.de>
2021-08-19 13:38:05 +0200

    edge node for hetzner

--- edge/edgecore/config.sh
+++ edge/edgecore/config.sh
@@ -2,7 +2,8 @@
 kubectl get secrets casecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cadata' > config/ca/rootCa.crt
 kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcoredata' > config/certs/server.crt
 kubectl get secrets cloudcoresecret -o jsonpath='{.data}' -n kubeedge | jq -r '.cloudcorekeydata' > config/certs/server.key
-IP=`kubectl get service cloudcore -n kubeedge -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+#IP=`kubectl get service cloudcore -n kubeedge -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+IP=kubeedge.tenant1.edgefarm.dev
 echo CLOUDCORE_IP=${IP} > template/values.conf
 TOKEN=`kubectl get secrets tokensecret -n kubeedge -o jsonpath='{.data}' | jq -r '.tokendata' | base64 -d`
 echo TOKEN="\"${TOKEN}\"" >> template/values.conf
